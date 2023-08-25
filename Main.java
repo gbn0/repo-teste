68c256ba1a7950de3ae2af97b67395d730c94a3e
gabrielpetry23 <gabriel.petry23@gmail.com>
2023-08-25 18:15:56 -0300

    Metodo b

--- Main.java
+++ Main.java
@@ -11,4 +11,15 @@ public class Main {
         }
         return n;
     }
+
+    public static boolean hasRepeat(ArrayList<Integer> l) {
+        for(int i = 0; i < l.size(); i++) {
+            for(int j = i + 1; j < l.size(); j++) {
+                if(l.get(i).equals(l.get(j))) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
 }
