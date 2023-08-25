90adb84eb71cc87ee9ec71fe4267077224ce7f81
Gabriel Saldanha <130391676+gbn0@users.noreply.github.com>
2023-08-25 17:31:59 -0300

    Método a

--- Main.java
+++ Main.java
@@ -1,6 +1,14 @@
+import java.util.ArrayList;
+
 public class Main {
 
-    public static void main(String[] args) {
-        
+    public static int nOcorrencias(ArrayList<Integer> l, Integer el) {
+        int n = 0;
+        for(int i = 0; i < l.size(); i++) {
+            if(l.get(i).equals(el)) {
+                n++;
+            }
+        }
+        return n;
     }
 }
