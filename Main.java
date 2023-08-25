83240a48a5823573df2abbbd2cadb799271ea53a
gabrielpetry23 <gabriel.petry23@gmail.com>
2023-08-25 18:38:29 -0300

    metodo e

--- Main.java
+++ Main.java
@@ -22,4 +22,20 @@ public class Main {
         }
         return false;
     }
+
+    public static ArrayList<Integer> uniao(ArrayList<Integer> l1, ArrayList<Integer> l2) {
+        boolean aux = false;
+        for (int i = 0; i < l2.size(); i++) {
+            for (int j = 0; i < l1.size(); i++) {
+                if (l1.get(j).equals(l2.get(i))) {
+                    aux = true;
+                }
+            }
+            if(!aux) {
+                l1.add(l2.get(i));
+            }
+            aux = false;
+        }
+        return l1;
+    }
 }
