cc572b7db75d398b77a28247c5ec15bde56928d0
Gabriel Saldanha <130391676+gbn0@users.noreply.github.com>
2023-08-25 18:19:29 -0300

    Metodo d

--- Main.java
+++ Main.java
@@ -11,4 +11,16 @@ public class Main {
         }
         return n;
     }
+
+    public static ArrayList<Integer> listRepeat(ArrayList<Integer> l) {
+        ArrayList<Integer> rep = new ArrayList<Integer>();
+        for(int i = 0; i < l.size(); i++) {
+            for(int j = i + 1; j < l.size(); j++) {
+                if(l.get(i).equals(l.get(j))) {
+                    rep.add(l.get(j));
+                }
+            }
+        }
+        return rep;
+    }
 }
